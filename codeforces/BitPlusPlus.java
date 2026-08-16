c84f158b224b78b8a227799ac736b0b845a23a56
Mayank Gupta <mayankguptasv01@gmail.com>
2026-08-16 14:52:55 +0530

    bitPlusPlus

---
+++ codeforces/BitPlusPlus.java
@@ -0,0 +1,22 @@
+package codeforces;
+
+import java.util.*;
+public class BitPlusPlus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+
+            if (s.contains("++")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        System.out.println(x);
+        sc.close();
+    }
+}
