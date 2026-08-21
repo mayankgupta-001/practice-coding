92380a5a641c6a1bcb75fdca488fc91ec906e8bf
Mayank Gupta <mayankguptasv01@gmail.com>
2026-08-21 10:58:18 +0530

    TwoDArrayDS

---
+++ hackerrank/TwoDArrayDS.java
@@ -0,0 +1,27 @@
+package hackerrank;
+import java.util.List;
+
+public class TwoDArrayDS {
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                int sum =
+                        arr.get(i).get(j)
+                      + arr.get(i).get(j + 1)
+                      + arr.get(i).get(j + 2)
+                      + arr.get(i + 1).get(j + 1)
+                      + arr.get(i + 2).get(j)
+                      + arr.get(i + 2).get(j + 1)
+                      + arr.get(i + 2).get(j + 2);
+
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
+    }
+}
